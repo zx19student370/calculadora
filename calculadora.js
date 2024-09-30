3f810d2466550809f31db52b141cc83f2bf1f47c
zx19student370 <zx19student370@colegiovalledelmiro.es>
2024-09-30 08:57:26 +0200

    Primera version

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+function operaciones(){
+    let num1 = parseInt(prompt("dime el primer numero"));
+    let num2 = parseInt(prompt("dime el segundo numero"));
+
+    let suma= num1 +num2;
+    console.log("Resultados:\n"+
+        "Suma: "+suma+"\n"
+
+    );
+}    
+
+operaciones();
