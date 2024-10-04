43b82e5853f7ba06da648a1a298083bb20666d15
zx19student370 <zx19student370@colegiovalledelmiro.es>
2024-10-04 09:12:09 +0200

    Modificacion funcion suma

--- calculadora.js
+++ calculadora.js
@@ -1,22 +1,14 @@
 function operaciones(){
     let num1 = parseInt(prompt("dime el primer numero"));
     let num2 = parseInt(prompt("dime el segundo numero"));
+    let num3 = parseInt(prompt("dime el tercer numero"));
 
-    let suma= num1 +num2;
-<<<<<<< HEAD
-    let resta= num1 - num2;
-    let division = num1 / num2;
-    console.log("Resultados:\n"+
-        "Suma: "+suma+"\n"+
-        "Resta: "+resta+"\n"+
-        "Division: "+division+"\n"
-=======
+    let suma= num1 +num2+num3;
     let division = num1/num2;
     console.log("Resultados:\n"+
         "Suma: "+suma+"\n",
         "Division: "+division+"\n"
 
->>>>>>> feature1
 
     );
 }    
